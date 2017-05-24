ea72f20f0b41865b3ff16017f75430a129a44299
Mikalai Lapatkin <Mikalai_Lapatkin@epam.com>
2017-05-24 14:33:37 +0300

    add bot messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -8,26 +8,28 @@ class Chat extends Component {
         this.updateMessage = this.updateMessage.bind(this);
         this.submitMessage = this.submitMessage.bind(this);
         this.enterSubmit = this.enterSubmit.bind(this);
+        this.addBotMessage = this.addBotMessage.bind(this);
 
         this.state = {
             message: '',
             messages: [
                 {
                     id: 0,
-                    text: 'Hello',
-                    className: 'message-own'
-                },
-                {
-                    id: 1,
-                    text: 'Where have you been before we met',
-                    className: 'message-own'
+                    text: 'Hello!',
+                    className: 'message-bot'
                 }
             ]
         }
     }
 
     botMessages = {
-        defaultResponse: 'Да, да, я вас слушаю'
+        defaultResponse: 'Да, да, я вас слушаю',
+        angryResponse: 'Пожалуйста, не сердитесь.',
+        questionResponse: [
+            'Простите, я затрудняюсь ответить',
+            'Сложный вопрос, здесь нужно подумать...',
+            'Не скажу, пусть это будет маленький секрет :)'
+        ]
     };
 
     updateMessage(event) {
@@ -37,7 +39,7 @@ class Chat extends Component {
     }
 
     submitMessage() {
-        if (this.state.message && this.state.message.length && ~this.state.message.search(/\S/)) {
+        if (this.state.message.length && ~this.state.message.search(/\S/)) {
             const nextMessage = {
                 id: this.state.messages.length,
                 text: this.state.message,
@@ -46,24 +48,59 @@ class Chat extends Component {
             let list = Object.assign([], this.state.messages);
             list.push(nextMessage);
 
-            let messageWithoutDots = this.state.message.replace(/\./g,'');
-            if (messageWithoutDots.search(/\S/) === -1) {
-                const botMessage = {
-                    id: this.state.messages.length + 1,
-                    text: this.botMessages.defaultResponse,
-                    className: 'message-bot'
-                };
-                list.push(botMessage);
-            }
+            let messageCopy = this.state.message.slice(0);
 
             this.setState({
-                messages: list
-            });
-
-            this.setState({
+                messages: list,
                 message: ''
             });
+
+            let objDiv = document.querySelector('.chat-field');
+            objDiv.scrollTop = objDiv.scrollHeight;
+
+            if (isNoContent(messageCopy)) {
+                this.addBotMessage(list, this.botMessages.defaultResponse, objDiv);
+            } else if (isShout(messageCopy)) {
+                this.addBotMessage(list, this.botMessages.angryResponse, objDiv);
+            } else if (isQuestion(messageCopy)) {
+                let randomAnswer = randomInteger(this.botMessages.questionResponse.length);
+                this.addBotMessage(list, this.botMessages.questionResponse[randomAnswer], objDiv);
+
+                function randomInteger(max) {
+                    let rand = Math.random() * max;
+                    rand = Math.floor(rand);
+                    return rand;
+                }
+            }
+
+            function isNoContent(message) {
+                let messageWithoutDots = message.replace(/\./g,'');
+                return messageWithoutDots.search(/\S/) === -1;
+            }
+
+            function isShout(message) {
+                return message.match(/[A-ZА-ЯЁ]{4,}/g) || (message.match(/[^\d\wа-яё]{2,}/gi) && !message.match(/[\d\wа-яё]/gi))
+            }
+
+            function isQuestion(message) {
+                return message.substr(-1) === '?';
+            }
         }
+    }
+
+    addBotMessage(list, message, div) {
+        setTimeout(() => {
+            const botMessage = {
+                id: list.length,
+                text: message,
+                className: 'message-bot'
+            };
+            list.push(botMessage);
+            this.setState({
+                messages: list,
+            });
+            div.scrollTop = div.scrollHeight;
+        }, 150);
     }
 
     enterSubmit(event) {
